aa3aa813bcb2687560a3bb1834b445f06f17f23c
Michalis Kamburelis <michalis.kambi@gmail.com>
2007-08-24 22:08:48 +0000

    * compile-release commands for programs changed, now they try to call compile.sh and similar scripts. This way I compile them the same way as other users, things like cleaning glwindow and specifying -dGLWINDOW_XLIB are already done, and kambi.cfg and unit paths are used relative to program's dir * malfunction fix to honour --display option
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/glplotter@1941 afe8dcc5-9094-451b-8773-5f495042a737

--- glplotter.dpr
+++ glplotter.dpr
@@ -898,7 +898,7 @@ end.
 
 {
   Local Variables:
-  kam-compile-release-command-win32: "clean_glwindow_unit && fpcreleaseb"
-  kam-compile-release-command-unix: "clean_glwindow_unit && fpcreleaseb"
+  kam-compile-release-command-unix:  "./compile.sh && mv -fv glplotter      ~/bin/"
+  kam-compile-release-command-win32: "./compile.sh && mv -fv glplotter.exe c:/bin/"
   End:
 }
